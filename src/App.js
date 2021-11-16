e7d5b12ee602a3f5c29a6c407b52239e125d7a16
Kristmund Ryggstein <kristmunds@hotmail.com>
2021-11-16 12:51:43 +0000

    uses federted logout

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { useAuth0 } from '@auth0/auth0-react';
 function App() {
   const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0()
 
-  console.log("user", user)
   return (
     <div className="App">
       <header className="App-header">
         <h1>Auth0 Test</h1>
         {isAuthenticated ? <div>
           <p>{user.email}</p>
-          <button onClick={() => logout()}>logout</button>
+          <button onClick={() => logout({
+            federated: true,
+          })}>logout</button>
         </div> : <button onClick={() => loginWithRedirect()}>login</button>}
       </header>
     </div>
